28286d63c77b44eb1cd854b4f7227532d96b10e2
Harish0311 <harishgamer00@gmail.com>
2023-03-18 11:36:41 +0530

    Learning_ML

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-import cv2 
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib
+import numpy as np
+
+dataset= tf.keras.datasets.fashion_mnist
+(train_images,train_label),(test_images,test_label )=dataset.load_data()
+index = 0 
+np.set_printoptions(linewidth=320)
+print(f'LABEL : {train_label[index]}')
+print(f'\nImage Pixel Array :\n {train_images[index]}')
